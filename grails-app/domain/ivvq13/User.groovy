c55bc3684273d615831925cfa26f9c510db8ecfa
loclamor <favier.loic.31@gmail.com>
2014-01-09 13:25:38 +0100

    Write domain class User. Close #1

--- grails-app/domain/ivvq13/User.groovy
+++ grails-app/domain/ivvq13/User.groovy
@@ -2,6 +2,19 @@ package ivvq13
 
 class User {
 
+	/*
+		+ id: long
+		+ username: String 
+		+ interests: Category[] 
+		+ publications: Document[] 
+		+ messages : Message[] 
+	 */
+	String username
+	static hasMany = [
+		interests : Category,
+		publications : Document,
+		messages : Message]
+	
     static constraints = {
     }
 	
