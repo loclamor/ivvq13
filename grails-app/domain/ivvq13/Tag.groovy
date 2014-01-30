2f290f6132a65856ee54f6d96f3aae8dd14feae7
VincentCarassus <vincentcarassus@gmail.com>
2014-01-30 11:56:01 +0100

    Update Tag tests #28

--- grails-app/domain/ivvq13/Tag.groovy
+++ grails-app/domain/ivvq13/Tag.groovy
@@ -8,7 +8,7 @@ class Tag {
 	static belongsTo = Document
 	
     static constraints = {
-		name size: 3..20, blank: false, unique: true
+		name size: 3..20, blank: false, unique: true, nullable: false
     }
 	
 }
