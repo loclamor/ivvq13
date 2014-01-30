d7b6f31b27dfc12e1c0999f6dd6a3c9dbebebf3c
VincentCarassus <vincentcarassus@gmail.com>
2014-01-30 16:39:22 +0100

    Classes are not nullable by default #28

--- grails-app/domain/ivvq13/Tag.groovy
+++ grails-app/domain/ivvq13/Tag.groovy
@@ -8,7 +8,6 @@ class Tag {
 	static belongsTo = Document
 	
     static constraints = {
-		name size: 1..20, blank: false, unique: true, nullable: false
+		name size: 1..20, blank: false, unique: true
     }
-	
 }
