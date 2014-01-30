255c34117a0e8ed03bdab3e8581cc6518d3f380a
VincentCarassus <vincentcarassus@gmail.com>
2014-01-30 10:50:39 +0100

    First tests on Tag constraints #28

--- test/unit/ivvq13/TagSpec.groovy
+++ test/unit/ivvq13/TagSpec.groovy
@@ -14,7 +14,37 @@ class TagSpec extends Specification {
 
     def cleanup() {
     }
-
-    void "test something"() {
-    }
+	
+	def "testCreation"() {
+		setup:
+		def tag = new Tag(name: "test")
+		def tags = [
+				new Tag(name: "foo"),
+				new Tag(name: "bar")]
+		
+		when:
+		tag.save()
+		tags*.save()
+		
+		then:
+		Tag.list().size() == 3
+	}
+	
+	def "testNoDouble"() {
+		setup:
+		Tag t1 = new Tag(name: "foo");
+		Tag t2 = new Tag(name: "foo");
+		
+		when:
+		t1.save()
+		t2.save()
+		
+		then:
+		Tag.list().size() == 1
+	}
+	
+	def "testNoBlankName"() {
+		Tag t1= new Tag(name: "");
+		
+	}
 }
