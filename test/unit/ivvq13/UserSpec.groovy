da534fe3eaaef49159f8812be67f3d4ae7aa9b5d
Raouf25 <makhlouf.raouf@gmail.com>
2014-02-02 23:46:46 +0100

    spock tests sur user #32

--- test/unit/ivvq13/UserSpec.groovy
+++ test/unit/ivvq13/UserSpec.groovy
@@ -12,26 +12,72 @@ class UserSpec extends Specification {
 	def user
 
 	def setup() {
+		mockForConstraintsTests(User, [new User(name: "123456789")])
+		user = new User(name:"thisIsAUser")
 	}
+	
+	def "testUserCreation"() {
+		setup:
+		def user = new User(username: "Name1",password :"password1",email :"email1")
+		def users = [
+				new User(username: "Name2",password :"password2",email :"email2"),
+				new User(username: "Name3",password :"password3",email :"email3")]
+		
+		when:
+		user.save()
+		users*.save()
+		
+		then:
+		assert User.list().size() == 3
+	}
+	
 
 	def "testNoNullUserName"() {
 		when:
-		def obj = new User(username: null)
-		obj.save()
-
+		def user0 = new User(username: "okName",password :"password0",email :"email0")	
+		def user1 = new User(username: null,password :"password0",email :"email0")
+		
+		user0.save()
+		user1.save()
+	
 		then:
-		assert !obj.validate()
+		assert user0.validate()
+		assert !user1.validate()
 	}
+	
 
 	def "testNoBlankUserName"() {
-		when:
-		def obj = new User(name: "")
-		obj.save()
-
+			when:
+		def user0 = new User(username: "okName",password :"password0",email :"email0")		
+		def user1 = new User(username: "",password :"password0",email :"email0")
+		def user2 = new User(username: "\t",password :"password0",email :"email0")
+		def user3 = new User(username: "\r",password :"password0",email :"email0")
+		
+		user0.save()
+		user1.save()
+		user2.save()
+		user3.save()
+	
 		then:
-		assert !obj.validate()
+		assert user0.validate()
+		assert !user1.validate()
+		assert !user2.validate()
+		assert !user3.validate()
 	}
 
+/*	def "testNotTwiceSameUsername"() {
+		when:
+		User user0 = new User(username: "name",password :"password0",email :"email0")
+		user0.save(flush: true)
+		User user1 = new User(username: "name",password :"password1",email :"email1")
+		user1.save(flush: true)
+						
+		then:
+		assert user0.validate()
+		assert !user1.validate()
+		assert User.list().size() == 1
+	}*/
+	
 	def cleanup() {
 	}
 
