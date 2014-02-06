1347c04c5864df7a39aef1cd26d3da94b8d6121d
alexburlacu11 <alexandru.burlacu11@gmail.com>
2014-02-06 15:36:25 +0100

    trimmed whitespace

--- grails-app/services/ivvq13/DocumentService.groovy
+++ grails-app/services/ivvq13/DocumentService.groovy
@@ -6,84 +6,6 @@ import grails.transaction.Transactional
 class DocumentService {
 
     def serviceCreateDocument(title_,category_,content_,tags_,attachments_,user_) {
-//		def res
-//		
-//		def c = new Category(name:category_);
-//		def a = new Attachment(url:attachments_)
-//		a.save()
-//		def m = new Message(content:"Welcome", author:user_, title:"Post messages here")
-//		m.save()
-////		def r = new Rating(value:1000, author:user_)
-////		r.save()
-//		def t = new Tag(name:"default")
-//		if(c.save(flush : true)) {
-//			println "We have a new category !";
-//		}
-//		else {
-//			println "Category already exists ";
-//		}
-//		//divide into tags
-//		def list_of_tags = tags_.split(",")
-//		def tagList = []
-//		list_of_tags.each {
-//			def tt = new Tag(name:it)			
-//			tagList.add(tt)
-//				if(!tt.save()){
-//					println("erreur enregistrement tag tt");
-//					tt.errors.allErrors.each( {e -> println (e) } )
-//				}
-//		}	
-//		
-//										
-//		//if the title is already in the database, please retry
-//		def doc = Document.findByTitleLike(title_)
-//		if( doc ) {
-//			println "A doc with this title already exists in the database";
-//			res = "/document/create_page"
-//			return res
-//		}
-//		else {
-//			//create new document and save in database
-//			def document = new Document(
-//				title:title_,
-//				category:c,
-//				user:user_,
-//				tags:t,
-//				attachments:a,
-//				messages:m,
-//				//ratings:r,
-//				content: content_
-//			)
-//			//add the tags
-//			println "PULA: "+tagList
-//			tagList.each  {								
-//				
-//				document.addToTags(it)
-//				
-//							
-//			}
-//			
-//			if(document.save()) {				
-//				tagList.each  {
-////				def ttt = Tag.findByName(it.name)
-////					if(ttt) {
-////						ttt.addToDocuments(document)
-////						ttt.save()
-////					}	
-//					it.addToDocuments(document)
-//					//it.save()			
-//				}
-//				println "Doc was saved"
-//				res = "/document/list"
-//				return res
-//			}
-//			else {
-//				println "Doc was not saved"
-//				res = "/document/create_page"
-//				return res
-//			}
-//		
-//		}
 		def res
 		
 		//if the title is already in the database, please retry
@@ -95,7 +17,8 @@ class DocumentService {
 		}
 	
 		//creation d'un document
-		def list_of_tags = tags_.split(",")
+		def st = tags_.trim()
+		def list_of_tags = st.split(",")
 		list_of_tags.each {
 			def tag1 = new Tag( name : it )
 			if(!tag1.save(flush:true)){
