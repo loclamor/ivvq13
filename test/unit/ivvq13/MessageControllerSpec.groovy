6285efb51e19c8d8a6fe12630384feea76d3f336
alexburlacu11 <alexandru.burlacu11@gmail.com>
2014-02-11 23:24:22 +0100

    tests, message controller and rating controller

--- test/unit/ivvq13/MessageControllerSpec.groovy
+++ test/unit/ivvq13/MessageControllerSpec.groovy
@@ -7,14 +7,54 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(MessageController)
+@Mock([Document,DocumentService,Attachment,Category,Message,Rating, RatingService, MessageService, UserService,Tag,User])
 class MessageControllerSpec extends Specification {
 
 	def setup() {
+		
+		def contentDoc = "Test document"
+		def contentMes = "Test message"
+		def tag1 = new Tag(name:"tag 1")
+		tag1.save()
+		def tag2 = new Tag(name:"tag 2")
+		tag2.save()
+		def category = new Category(name:"Grails")
+		category.save()
+		def attachment = new Attachment(name: "www.google.com")
+		attachment.save()
+		def user = new User(username: "alex", password: "a", email:"alex@a.com")
+		user.save()
+		def rating = new Rating(author: user, value: 10)
+		rating.save()
+		def message = new Message(content: contentMes, title:"Hello", author:user)
+		message.save()
+		def doc = new Document()
+		doc.setTitle("my title")
+		doc.setUser(user)
+		doc.setCategory(category)
+		doc.addToTags(tag1)
+		doc.addToTags(tag2)
+		doc.addToMessages(message)
+		doc.setContent(contentDoc)
+		doc.addToRatings(rating)
+		doc.addToAttachments(attachment)
+		doc.save()
 	}
 
 	def cleanup() {
 	}
 
-	void "test something"() {
+	void "Message Controller : add message"() {
+		setup:
+		params.inputTitle = "Hi"
+		params.inputContent = "How are you ?"
+		def mid = Document.findByTitle("my title")
+		
+		when:
+		controller.add(mid.id)
+		
+		then:
+		assert response.redirectedUrl == "/document/view/1"
+		
 	}
 }
--- test/unit/ivvq13/RatingControllerSpec.groovy
+++ test/unit/ivvq13/RatingControllerSpec.groovy
@@ -7,14 +7,54 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(RatingController)
+@Mock([Document,Attachment,Category,Message,Rating, RatingService, MessageService, UserService,Tag,User])
 class RatingControllerSpec extends Specification {
 
 	def setup() {
+		
+		def contentDoc = "Test document"
+		def contentMes = "Test message"
+		def tag1 = new Tag(name:"tag 1")
+		tag1.save()
+		def tag2 = new Tag(name:"tag 2")
+		tag2.save()
+		def category = new Category(name:"Grails")
+		category.save()
+		def attachment = new Attachment(name: "www.google.com")
+		attachment.save()
+		def user = new User(username: "alex", password: "a", email:"alex@a.com")
+		user.save()
+		def rating = new Rating(author: user, value: 10)
+		rating.save()
+		def message = new Message(content: contentMes, title:"Hello", author:user)
+		message.save()
+		def doc = new Document()
+		doc.setTitle("my title")
+		doc.setUser(user)
+		doc.setCategory(category)
+		doc.addToTags(tag1)
+		doc.addToTags(tag2)
+		doc.addToMessages(message)
+		doc.setContent(contentDoc)
+		doc.addToRatings(rating)
+		doc.addToAttachments(attachment)
+		doc.save()
+		
 	}
 
 	def cleanup() {
 	}
 
-	void "test something"() {
-	}
+//	void "Rating Controller : add rate"() {
+//		setup:
+//		params.userRate = 3
+//		def mid = Document.findByTitle("my title")
+//		
+//		when:
+//		controller.rate(mid.id)
+//		
+//		then:
+//		assert response.redirectedUrl == "/document/view/1"
+//		
+//	}
 }
